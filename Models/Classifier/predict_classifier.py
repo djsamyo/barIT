00b0abe7c8b1f5563af5d0689b9b6a0f05c57879
prateetisaran <62251268+prateetisaran@users.noreply.github.com>
2022-07-25 01:52:43 +0400

    Update predict_classifier.py

--- Models/Classifier/predict_classifier.py
+++ Models/Classifier/predict_classifier.py
@@ -1,1 +1,13 @@
+from train_classifier import *
 
+def run_classifier(image): 
+  if not os.path.exists('Models/classifier.h5'): 
+    retrain_classifier()
+  model = keras.models.load_model('Models/classifier.h5')
+  
+  pred = model.predict(image)
+  if pred[1] > 0.5: 
+    return True
+  else: 
+    return False
+  
