ada20d9e683a2ae77cb561d19a71a5d88d3d73c5
prateetisaran <62251268+prateetisaran@users.noreply.github.com>
2022-07-25 01:50:21 +0400

    Update train_classifier.py

--- Models/Classifier/train_classifier.py
+++ Models/Classifier/train_classifier.py
@@ -9,12 +9,12 @@ def get_classifier_model():
     classifier_activation="softmax",
 )
 
+def retrain_classifier(): 
+  X = np.load()#Figure out pathname
+  Y_classifier = np.load() #Figure out pathname
 
-X = np.load()#Figure out pathname
-Y_classifier = np.load() #Figure out pathname
+  model = get_classifier_model()
+  model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy')
+  model.fit(X, Y_classifier, epochs = 150)
 
-model = get_classifier_model()
-model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy')
-model.fit(X, Y_classifier, epochs = 150)
-
-model.save("Models/classifier")
+  model.save("Models/classifier")
